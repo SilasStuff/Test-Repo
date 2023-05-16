31c002075d5efbce1d8cea91b3715867496f239c
Silas <silas.yeatman@gmail.com>
2023-05-16 16:18:50 +0100

    Test commit

--- R experiments.R
+++ R experiments.R
@@ -1,3 +1,5 @@
-SoTN_2023<- read_excel("SoTN/SoTN 2023 - Initial Analysis - Updated 02 May 2023.xlsx")
-View(SoTN_2023_Initial_Analysis_Updated_02_May_2023)
+library(readxl)
+SoTN_2023 <- read_excel("SoTN 2023 - Initial Analysis - Updated 02 May 2023.xlsx", sheet = "Data", range = "A4:HN54")
 
+
+View(SoTN_2023)
